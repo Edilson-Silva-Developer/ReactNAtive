8ce21ffd6fc8ded9fcea2e86085bae4296fbe6da
Edilson <edilsonantonio0@gmail.com>
2023-02-23 07:00:29 -0300

    aula icompleta curso mouse

---
+++ JavaScript/script6.js
@@ -0,0 +1,36 @@
+//Apertou o mouse
+
+
+function apertoumouse() {
+
+    console.log("o mouse foi apertado!");
+
+}
+
+//mouse up
+
+function solteiomouse() {
+
+    console.log("soltei o botão do mouse");
+}
+
+//passou o mouse por cima do botão
+
+function passouomouse() {
+
+    console.log("Passou o mouse por cima do botão");
+}
+
+//tirou o mouse de cima do botão
+
+function tirouomouse() {
+
+    console.log("Tirou o mouse de cima do botão");
+}
+
+//movendo o mouse
+
+function movendoMouse() {
+
+    console.log("Voce esta movendo o mouse");
+}
