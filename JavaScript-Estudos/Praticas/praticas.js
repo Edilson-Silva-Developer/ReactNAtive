09f5623b25c30f9ca8a43a1fafa0fcdf3bd42702
Edilson <edilsonantonio0@gmail.com>
2023-02-27 22:13:16 -0300

    Patricas feita até booleans e comparações

---
+++ JavaScript-Estudos/Praticas/praticas.js
@@ -0,0 +1,38 @@
+//função numerica com tipos de dados 
+console.log(typeof 5 + typeof 12); 
+console.log(8 - 2);
+console.log(10 / 2);
+console.log(8 * 2);
+console.log(25 % 2);
+
+
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
+
+console.log(2*4-10);
+console.log(5+(4*4));
+
+console.log("Edilson Antonio");// com aspas duplas
+console.log('Edilson silva');//com aspas simples.
+console.log(`Edilson`);//backticks nas string funciona normal
+
+
+//String com concatenação, espaços, pulando linhas.
+console.log("Ola\ \ mundo\nHelo World");
+console.log('Edilson e "Juliana"');
+console.log("Juliana e 'Edilson'");
+console.log(`A soma de 4 + 4 é ${4+4}`); //a soma  de numero co, a string só funciona usando backtick.
+console.log("Edilson " + "e" + " Juliana");//concatenação de nomes.
+console.log("Ola"+" Mundo" + "\nHola \ \ world " + 'Java '+ " Script\n" + `A soma de 5 + 5 é ${5+5} ` + "\nReact 'Native'" );
+
+console.log(10>2);
+console.log(200>100);
+
+console.log(5>2);//maior que
+console.log(10<11);//menor que 
+console.log(5>=6);//maior que ou igual
+console.log(10<=11);//menor que ou igual
+console.log(10==11);//igual
+console.log(10 != 1);//diferente
+console.log(5===5);//indentico 
