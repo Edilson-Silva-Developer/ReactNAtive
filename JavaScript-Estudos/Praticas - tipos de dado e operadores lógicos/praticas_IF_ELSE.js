c3e511882aa7e99238cdffeaa5d58f9b410f0d8e
Edilson Silva <104467452+Edilson-Silva-Developer@users.noreply.github.com>
2023-03-06 22:06:07 -0300

    idade

---
+++ JavaScript-Estudos/Praticas - tipos de dado e operadores lógicos/praticas_IF_ELSE.js
@@ -0,0 +1,27 @@
+let numeroS = 31;
+
+ if(numeroS == 33){
+
+    console.log("Idade de Cristo");
+ }else{
+
+    console.log("Falta muito ou já passou");
+ }
+
+
+
+
+ let idade = prompt("Qual a sua idade ? ");
+
+ if(idade <= 17){
+    console.log(alert(`Menor de Idade, sua idade é ${idade}`));
+ }else if( idade <= 59){
+    console.log(alert(`É maior de idade, sua idade é: ${idade}`));
+ }else if (idade >= 60){
+   console.log(alert(`Você é um acião, sua idade é ${idade}`));
+ }
+
+
+
+
+ 
