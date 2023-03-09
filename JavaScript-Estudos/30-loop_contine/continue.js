ac27b46e025731614f5327db1473fb5c162cf9a6
Edilson Silva <104467452+Edilson-Silva-Developer@users.noreply.github.com>
2023-03-08 21:03:07 -0300

    loop

---
+++ JavaScript-Estudos/30-loop_contine/continue.js
@@ -0,0 +1,12 @@
+//função faz o loop e depois continua 
+for (let i = 10; i > 0; i = i -1){ 
+
+    if(i % 2 == 0){
+      console.log("Caiu no continue");
+      continue;  
+
+    }
+
+    console.log(i);
+
+}
