fe709d96a19cd8d765a71bf4dbcf24713c0d8337
Edilson Silva <104467452+Edilson-Silva-Developer@users.noreply.github.com>
2023-04-07 21:48:36 -0300

    ok

---
+++ logicadeprogrmacaojavascript/variavel.js
@@ -0,0 +1,43 @@
+var nome  = 'Edilson';
+var nome = 'Julina';
+    nome = 'Silva';
+
+    console.log(nome);
+
+
+let nome2 = 'Paulino';
+    nome2 = 'Aantonio'//pode ser reatribuida o a variavel LET
+
+    console.log(nome2);
+
+
+const date = 1989;//usar o consta quando a informação não for alterada.
+
+
+    console.log(date);
+
+
+
+
+/**
+ * length = inicia do 1
+ * indice =  incia do 0
+ */  
+const array1  = ["carro","Casa","bicilceta","moto"];//array é grupo de objeto
+    console.log(typeof array1);
+    console.log(array1);
+    console.log(array1.length);//posição na array
+
+
+
+const obj02 = [
+    {nome:"Edilson"},
+    {nome:"Juliana"},
+    {nome:"Silva"}
+];
+
+console.log(obj02);
+console.log(obj02[0]);
+
+
+
