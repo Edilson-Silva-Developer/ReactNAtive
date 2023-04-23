de7cfd520e0f7d33cf8d828cbbd39c26149f7f9a
Edilson Silva <104467452+Edilson-Silva-Developer@users.noreply.github.com>
2023-04-23 12:17:46 -0300

    ok

--- JavaScript-Estudos/85-Assertions/assertions.js
+++ JavaScript-Estudos/85-Assertions/assertions.js
@@ -1,29 +1,29 @@
 //assertions
 
-let arr = [1,2,3,65,];
-let arr2 = [ ];
+let arr = [1, 2, 3, 65,];
+let arr2 = [];
 
- function interarArray(arr){
-    if(arr.length == 0){
+function interarArray(arr) {
+    if (arr.length == 0) {
         throw new Error("O arrya precisa ter pelo menso um elemento");
-    }else{
-        for (let i = 0; i < arr.length; i++){
+    } else {
+        for (let i = 0; i < arr.length; i++) {
             console.log(i);
 
         }
     }
- }
+}
 
 
- function arryVazio(arr){
-    if (arr.length > 0){
-    throw Error ("O array não pode tee elementos");
+function arryVazio(arr) {
+    if (arr.length > 0) {
+        throw Error("O array não pode tee elementos");
 
-    }else{
+    } else {
         console.log("Agora deu certo");
     }
- }
+}
 
 
- interarArray(arr);
- interarArray(arr2);
+interarArray(arr);
+interarArray(arr2);
